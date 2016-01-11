6e5a74374976b371e3f677041c4c96448d8c666f
andyvuong <andyv.42@gmail.com>
2016-01-11 15:36:48 -0600

    Fix port

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.post("/crawl", function(req, res) {
 });
 
 // Server Code
-var server = app.listen(8000, function() {
+var server = app.listen(process.env.PORT || 8000, function() {
     var host = server.address().address;
     var port = server.address().port;
     console.log("Example app listening at http://%s:%s", host, port);
